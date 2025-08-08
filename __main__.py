50d12db024998ff7195cef387b8cb2eab7e78630
skr2005 <62237294+skr2005@users.noreply.github.com>
2025-08-08 20:34:57 +0800

    feat: 初步完成五个参数计算

---
+++ __main__.py
@@ -0,0 +1,2 @@
+from .prelude import *
+from .parameters import *
---
+++ parameters.py
@@ -0,0 +1,25 @@
+from .prelude import *
+from .data_abstract.spectrum_abstract import Spectrum
+
+
+def calc_all_5_parameters(
+    spectrum: Spectrum,
+) -> tuple[
+    np.float64,
+    np.float64,
+    np.float64,
+    np.float64,
+    np.float64,
+]:
+    """
+    根据光谱计算出：
+    CCT, Duv, Rg, Rf, melDER
+    """
+
+    stacked = np.vstack((spectrum.wavelength(), spectrum.spd()))
+
+    Rf, Rg, CCT, Duv = cast(Any, spd_to_iesrf(stacked, "Rf,Rg,cct,duv"))
+
+    melDER = spd_to_aopicDER(stacked)[:, -1]
+
+    return CCT[0][0], Duv[0][0], Rg[0][0], Rf[0][0], melDER[0][0]
