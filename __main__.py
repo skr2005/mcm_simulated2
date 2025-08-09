503832e9a90f8a26dbd6ff5b1617cf2c55e3560e
lhkccccccv <2228357305@qq.com>
2025-08-09 19:05:07 +0800

    场景二

--- __main__.py
+++ __main__.py
@@ -2,10 +2,14 @@ from .prelude import *
 from .parameters import calc_all_5_parameters
 from .data.problem1 import SpectrumProblem1
 from .data.problem2 import Problem2Data
-from .problem2 import scenario_1
+from .problem2 import scenario_1,scenario_2
 
 
-def main(): ...
+def main(): 
+    # p2 = Problem2Data()
+    # result = scenario_2(p2)
+    # print(f"目标函数结果:{result.fun}")
+    # print(f"决策变量:{result.x}")
 
 
 if __name__ == "__main__":
