d5aed568bf3c04b75639835961f9103f2e4d9055
skr2005 <62237294+skr2005@users.noreply.github.com>
2025-08-09 22:49:33 +0800

    feat: 第三问数据抽象 (#53)

---
+++ data/problem3.py
@@ -0,0 +1,13 @@
+from ..data_abstract.spectrum_abstract import Spectrum
+
+
+class Problem3Data:
+    def __init__(self) -> None: ...  # TODO
+
+    def spectra(self) -> list[Spectrum]:
+        """
+        返回按照时间顺序的光谱列表
+
+        预期时间复杂度：O(1)
+        """
+        ...  # TODO
