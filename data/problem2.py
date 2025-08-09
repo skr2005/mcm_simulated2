e49799fea2ec5acc4034ef3ca81d08e35326dfec
skr2005 <62237294+skr2005@users.noreply.github.com>
2025-08-09 13:42:24 +0800

    feat: 第二问的数据的类结构

---
+++ data/problem2.py
@@ -0,0 +1,17 @@
+from ..prelude import *
+from ..data_abstract.spectrum_abstract import Spectrum
+
+
+class Problem2Data:
+    def __init__(self) -> None: ...  # TODO
+
+    def five_leds(self) -> list[Spectrum]:
+        """
+        返回五种可用LED的光谱
+
+        要求每次调用该函数返回的结果是相同的。
+        要求五种LED的`Spectrum`的`wavelength`数组相同，可以让它们指向同一个数组。
+
+        期望时间复杂度O(1)
+        """
+        ...  # TODO
