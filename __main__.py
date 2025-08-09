4c7eab5a5a5d2bb1ac553afdca169ae1a44b0cf8
aiming graduate <162136259+lhkccccccv@users.noreply.github.com>
2025-08-09 16:44:33 +0800

    feat: 实例化第二问 (#42)
    * 这个是实例化的测试，我该怎么使用才能打印出结果呢？
    
    * 实例化成功Problem2
    
    * 修正
    
    * Update data/problem2.py
    
    Co-authored-by: skr2005 <62237294+skr2005@users.noreply.github.com>
    
    * 转换精度+导入依赖
    
    * Update problem2.txt
    
    ---------
    
    Co-authored-by: skr2005 <62237294+skr2005@users.noreply.github.com>

--- __main__.py
+++ __main__.py
@@ -1,3 +1,11 @@
 from .prelude import *
 from .parameters import calc_all_5_parameters
 from .data.problem1 import SpectrumProblem1
+from .data.problem2 import Problem2Data
+
+def main():
+    ...
+
+
+if __name__ =="__main__":
+    main()
