66f2434e1210b78fc0a2eff393188df3498aaed5
lhkccccccv <2228357305@qq.com>
2025-08-09 15:15:34 +0800

    这个是实例化的测试，我该怎么使用才能打印出结果呢？

--- __main__.py
+++ __main__.py
@@ -1,3 +1,11 @@
 from .prelude import *
 from .parameters import calc_all_5_parameters
 from .data.problem1 import SpectrumProblem1
+from .data.problem2 import Init_classes
+
+def main():
+    Init_classes()
+
+
+if __name__ =="__main__.py":
+    main()
--- data/problem2.py
+++ data/problem2.py
@@ -1,6 +1,22 @@
+from numpy import float64
+from mcm_simulated2.prelude import NDArray
 from ..prelude import *
 from ..data_abstract.spectrum_abstract import Spectrum
+from . import DATA_RAW_PATH
+import pandas as pd
+from pprint import pprint
 
+__all__ = ["Init_classes"]
+
+class SpectrumProblem2(Spectrum):
+    def __init__(self,led_num,arr1,arr2) ->None:
+        self.n = led_num
+        self.spd_data = arr1
+        self.wavelength_data = arr2
+    def spd(self)->NDArray[np.float64]:
+        return self.spd_data
+    def wavelength(self) -> NDArray[np.float64]:
+        return self.wavelength_data
 
 class Problem2Data:
     def __init__(self) -> None: ...  # TODO
@@ -15,3 +31,15 @@ class Problem2Data:
         期望时间复杂度O(1)
         """
         ...  # TODO
+
+def Init_classes():
+    print("stsagas")
+    data = pd.read_csv(DATA_RAW_PATH / "problem2.txt",sep=" ",header=0,encoding="utf-8")
+    wave_length = data.iloc[:,0]
+    five_leds = []
+    for i in range(len(data.columns)-1):
+        five_leds.append(SpectrumProblem2(i,data.iloc[:,i+1],wave_length))
+    pprint(five_leds)
+
+
+
