b1f44c29802be2f9751377a6ec704165210898b6
lhkccccccv <2228357305@qq.com>
2025-08-10 14:01:48 +0800

    最小二乘

--- problem3.py
+++ problem3.py
@@ -1,5 +1,8 @@
+from numpy import dtype
 from .prelude import *
+from .data.problem2 import Problem2Data
 from .data.problem3 import Problem3Data
+from .problem2 import combine_spd
 from .data_abstract.spectrum_abstract import Spectrum
 from .parameters import calc_all_5_parameters
 
@@ -7,6 +10,9 @@ plt.rcParams["font.family"] = ["SimHei"]
 
 
 def p3_plot_melDER(data3: Problem3Data):
+    """
+    画出目标melDER关于时间的散点图
+    """
     y = []
     for spectrum in data3.spectra():
         stacked = np.vstack((spectrum.wavelength(), spectrum.spd()))
@@ -23,29 +29,41 @@ def p3_plot_melDER(data3: Problem3Data):
     plt.show()
 
 
-def p3_least_squares(data3: Problem3Data):
-    x = np.array([i + 5.5 for i in range(len(data3.time_info()))], dtype=float)
-    y = []
+def p3_solution(data2: Problem2Data, data3: Problem3Data):
+    """
+    使用最小二乘法衡量拟合效果
+    """
+    # y为目标mel-DER
+    y_target = []
+    arr = []
     for spectrum in data3.spectra():
         stacked = np.vstack((spectrum.wavelength(), spectrum.spd()))
+        arr.append(spectrum.spd())
         melDER = spd_to_aopicDER(stacked)[:, -1]
-        y.append(melDER[0])
-    y = np.array(y, dtype=float)
+        y_target.append(melDER[0])
+    y_target = np.array(y_target, dtype=float)
+    arr = np.array(arr, dtype=float)
 
-    def residuals(params, x, y):
-        a, b, c, d = params
-        model = np.polyval([a, b, c, d], x)
-        return model - y  # 返回残差（拟合值 - 观测值）
+    # 五个leds的spectrum
+    leds = []
+    for spectrum in data2.five_leds():
+        leds.append(spectrum.spd())
+    leds = np.array(leds, dtype=float)
 
-    initial_params = [0.2, 0.4877, 8.3306, 0]
+    wl = data2.five_leds()[0].wavelength()
 
-    result = least_squares(residuals, initial_params, args=(x, y))
+    for i in range(len(arr)):
 
-    a1,b1,c1,d1 = result.x
-    print(result.x)
+        def residuals(w):
+            w = np.exp(w)
+            w = w / w.sum()
+            spd = combine_spd(data2.five_leds(), w)
+            res = spd - arr[i]
+            return res.flatten()
 
-    y_fit =  y_fit = np.polyval([a1,b1,c1,d1], x)
-    plt.scatter(x, y)
-    plt.plot(x, y_fit, "r-", label="拟合函数")
-    plt.legend()
-    plt.show()
+        w = np.zeros(5)
+
+        result = least_squares(residuals, w)
+        result.x = np.exp(result.x)
+        result.x = result.x / result.x.sum()
+        print(result.x)
